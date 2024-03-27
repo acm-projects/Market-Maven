d13f80b11fb2f5615879b992485038a985ef6ad9
rehmanalveena <rehman.alveena@gmail.com>
2024-03-27 12:58:17 -0500

    Shop can now fetch products from db

--- frontend/market-maven/src/Components/Item.js
+++ frontend/market-maven/src/Components/Item.js
@@ -8,10 +8,10 @@ const Item = (props) => {
     <>
     <div className="p-4 my-2 rounded-lg flex flex-col items-center">
       <div className="overflow-hidden rounded-xl shadow-xl my-4">
-        <img className='w-[300px] h-[350px] object-cover bg-blue-500 hover:scale-110 duration-300' src={props.pic} alt={props.name} />
+        <img className='w-[300px] h-[350px] object-cover bg-blue-500 hover:scale-110 duration-300' src={props.image} alt={props.name} />
         <div className='bg-white w-[300px] p-2'>
       <h1 className='text-3xl text-black text-center'>{props.name}</h1>
-      <h2 className='text-2xl'>{props.cost}</h2>
+      <h2 className='text-2xl'>{props.price}</h2>
       </div>
       </div>
       
