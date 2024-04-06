91e9b5d4950fc2bcc1117ce95e5a229afa637f80
rehmanalveena <rehman.alveena@gmail.com>
2024-04-06 00:46:57 -0500

    shop page integrated with product endpoints

--- frontend/market-maven/src/Components/ItemDetails.js
+++ frontend/market-maven/src/Components/ItemDetails.js
@@ -7,48 +7,61 @@ const ItemDetails = () => {
   const [item, setItem] = useState({}); // Initialize item state with an empty object
 
   // takes the product id from the Link routing as a parameter
-  const params = useParams ();
+  const params = useParams();
 
   // useEffect executse on component mount (no compile errors)
   useEffect(() => {
-    console.log('useEffect Item Details');
+    console.log("useEffect Item Details");
     // GET request to fetch the single product
     const fetchProduct = async () => {
-      const res = await axios.get(`http://localhost:8080/api/products/${params.id}`);
-      console.log(res.data)
-      
+      const res = await axios.get(
+        `http://localhost:8080/api/products/${params.id}`
+      );
+      console.log(res.data);
+
       setItem(res.data);
     };
 
-    fetchProduct()
+    fetchProduct();
   }, []);
 
   // Handle click event for adding item to cart
-  const handleClick = (name, quantity) => {
-
-  };
+  const handleClick = (name, quantity) => {};
 
   return (
     <>
       <Navbar />
-      <div className="grid grid-cols-2 bg-white">
-        <div className="object-left h-[600px] w-[600px] bg-white">
-          {/* Render item image */}
-          <img src={item.image} alt={item.productTitle} />
-        </div>
-        <div className="">
-          <h1 className="text-center text-black">{item.productTitle}</h1>
-          <h2>Price: {item.price}</h2>
-          <h2>Stock: {item.stock}</h2>
-          <p className="my-4">Description: {item.description}</p>
-
-          {/* Render Add to Cart button */}
-          <button
-            onClick={() => handleClick(item.productTitle, item.stock)}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Add to Cart
-          </button>
+      <div class="bg-gray-100 py-8">
+        <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="flex flex-col md:flex-row -mx-4">
+                <div class="md:flex-1 px-4">
+                    <div class="h-[460px] rounded-lg bg-gray-200 dark:bg-gray-200 mb-4">
+                        <img class="w-full h-full object-cover" src={item.image} alt={item.productTitle}/>
+                    </div>
+                    <div class="flex -mx-2 mb-4">
+                        <div class="w-full px-2">
+                            <button class="w-full bg-gray-900 dark:bg-gray-500 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-500 dark:hover:bg-gray-700">Add to Cart</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="md:flex-1 px-4">
+                    <h2 class="text-2xl font-bold text-gray-800 dark:text-gray-400 mb-2">{item.productTitle}</h2>
+                    <div class="flex mb-4">
+                        <div class="mr-4">
+                            <span class="font-bold text-gray-700 dark:text-gray-400">Price:</span>
+                            <span class="text-gray-600 dark:text-gray-400"> ${item.price}</span>
+                        </div>
+                        <div>
+                            <span class="font-bold text-gray-700 dark:text-gray-400">Stock:</span>
+                            <span class="text-gray-600 dark:text-gray-400"> {item.stock}</span>
+                        </div>
+                    </div>
+                    <div>
+                        <span class="font-bold text-gray-700 dark:text-gray-400">Product Description:</span>
+                        <p class="text-gray-600 dark:text-gray-400 text-sm mt-2">{item.description}</p>
+                    </div>
+                </div>
+            </div>
         </div>
       </div>
     </>
