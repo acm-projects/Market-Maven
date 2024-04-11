84796dd1895493975d500d682656c1991dce90c5
rehmanalveena <rehman.alveena@gmail.com>
2024-04-11 17:23:14 -0500

    item details shows reviews. Need to work on UI

--- frontend/market-maven/src/Components/ItemDetails.js
+++ frontend/market-maven/src/Components/ItemDetails.js
@@ -5,6 +5,7 @@ import axios from "axios";
 
 const ItemDetails = () => {
   const [item, setItem] = useState({}); // Initialize item state with an empty object
+  const [reviews, setReviews] = useState([]); // Initialize reviews state with an empty array
 
   // takes the product id from the Link routing as a parameter
   const params = useParams();
@@ -18,8 +19,14 @@ const ItemDetails = () => {
         `http://localhost:8080/api/products/${params.id}`
       );
       console.log(res.data);
-
       setItem(res.data);
+
+      // Fetch reviews associated with the item
+      const reviewsResponse = await axios.get(
+        `http://localhost:8080/api/reviews?product=${params.id}`
+      );
+      console.log(reviewsResponse.data);
+      setReviews(reviewsResponse.data);
     };
 
     fetchProduct();
@@ -31,37 +38,63 @@ const ItemDetails = () => {
   return (
     <>
       <Navbar />
-      <div class="bg-gray-100 py-8">
+      {/* grid grid-cols-2 auto-rows-[300px] */}
+      <div class="bg-gray-100 py-8 ">
         <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div class="flex flex-col md:flex-row -mx-4">
-                <div class="md:flex-1 px-4">
-                    <div class="h-[460px] rounded-lg bg-gray-200 dark:bg-gray-200 mb-4">
-                        <img class="w-full h-full object-cover" src={item.image} alt={item.productTitle}/>
-                    </div>
-                    <div class="flex -mx-2 mb-4">
-                        <div class="w-full px-2">
-                            <button class="w-full bg-gray-900 dark:bg-gray-500 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-500 dark:hover:bg-gray-700">Add to Cart</button>
-                        </div>
-                    </div>
+          <div class="flex flex-col md:flex-row -mx-4">
+            {/* ITEM IMAGES */}
+            <div class="md:flex-1 px-4">
+              <div class="h-[460px] rounded-lg bg-gray-200 dark:bg-gray-200 mb-4">
+                <img
+                  class="w-full h-full object-cover"
+                  src={item.image}
+                  alt={item.productTitle}
+                />
+              </div>
+              <div class="flex -mx-2 mb-4">
+                <div class="w-full px-2">
+                  <button class="w-full bg-gray-900 dark:bg-gray-500 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-500 dark:hover:bg-gray-700">
+                    Add to Cart
+                  </button>
                 </div>
-                <div class="md:flex-1 px-4">
-                    <h2 class="text-2xl font-bold text-gray-800 dark:text-gray-400 mb-2">{item.productTitle}</h2>
-                    <div class="flex mb-4">
-                        <div class="mr-4">
-                            <span class="font-bold text-gray-700 dark:text-gray-400">Price:</span>
-                            <span class="text-gray-600 dark:text-gray-400"> ${item.price}</span>
-                        </div>
-                        <div>
-                            <span class="font-bold text-gray-700 dark:text-gray-400">Stock:</span>
-                            <span class="text-gray-600 dark:text-gray-400"> {item.stock}</span>
-                        </div>
-                    </div>
-                    <div>
-                        <span class="font-bold text-gray-700 dark:text-gray-400">Product Description:</span>
-                        <p class="text-gray-600 dark:text-gray-400 text-sm mt-2">{item.description}</p>
-                    </div>
-                </div>
+              </div>
             </div>
+
+            {/* ITEM DETAILS */}
+            <div class="md:flex-1 px-4">
+              <h2 class="text-2xl font-bold text-gray-800 dark:text-gray-400 mb-2"> {item.productTitle} </h2>
+              <div class="flex mb-4">
+                <div class="mr-4">
+                  <span class="font-bold text-gray-700 dark:text-gray-400"> Price: </span>
+                  <span class="text-gray-600 dark:text-gray-400"> {" "} ${item.price} </span>
+                </div>
+                <div>
+                  <span class="font-bold text-gray-700 dark:text-gray-400"> Stock: </span>
+                  <span class="text-gray-600 dark:text-gray-400"> {" "} {item.stock} </span>
+                </div>
+              </div>
+              <div>
+                <span class="font-bold text-gray-700 dark:text-gray-400"> Product Description: </span>
+                <p class="text-gray-600 dark:text-gray-400 text-sm mt-2"> {item.description} </p>
+              </div>
+            </div>
+          </div>
+          {/* REVIEWS */}
+          <div>
+              <h2 class="text-2xl font-bold text-gray-800 dark:text-gray-400 mb-2"> Reviews </h2>
+              <div>
+                <ul>
+                  {reviews.map((review) => {
+                    console.log("Review:", review);
+                    return (
+                      <li key={review._id}>
+                        <p>{review.thereview}</p>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+          </div>
         </div>
       </div>
     </>
--- frontend/market-maven/src/Components/Review.js
+++ frontend/market-maven/src/Components/Review.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { useState } from "react";
 
-const Item = (props) => {
+const Review = (props) => {
   return (
     <>
       <div className="p-4 my-2 rounded-lg flex flex-col items-center">
         <div className="overflow-hidden rounded-xl shadow-xl my-4">
-          <img
+          {/* user profile picture */}
+          {/* <img
             className="w-[300px] h-[350px] object-cover bg-blue-500 hover:scale-110 duration-300"
             src={props.image}
             alt={props.name}
-          />
+          /> */}
+
           <div className="bg-white w-[300px] p-2">
-            <h1 className="text-3xl text-black text-center">{props.name}</h1>
-            <h2 className="text-2xl">{props.price}</h2>
+            <h1 className="text-3xl text-black text-center">{props.thereview}</h1>
           </div>
         </div>
       </div>
@@ -21,4 +22,4 @@ const Item = (props) => {
   );
 };
 
-export default Item;
+export default Review;
